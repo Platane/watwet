9d69a62ad63713624c0eaaec1a866d00b278fc13
platane <me@platane.me>
2018-03-29 00:17:57 +0100

    :rocket: better local storage

--- src/sideEffect/localStorage.js
+++ src/sideEffect/localStorage.js
@@ -1,27 +1,69 @@
 import { write, read } from '~/service/localStorage'
 import { selectCurrentUser } from '~/store/selector/currentUser'
 import { read as readAction } from '~/store/action/localStorage'
+import { debounce, wait } from '~/util/time'
 
-const extractStorableResource = ({
-  original,
-  mutated,
-  dateFetched,
-  dateMutated,
-}) => ({ original, mutated, dateFetched, dateMutated })
+const createPersist = (label, selector) => {
+  let persisted = null
+
+  return state => {
+    const x = selector(state)
+
+    if (x !== persisted) {
+      console.log('persist', label)
+      persisted = x
+      write(label, x)
+    }
+  }
+}
+
+const createPersistObject = (rootLabel, o) => {
+  const createPersistTree = (label, o) =>
+    typeof o === 'function'
+      ? [createPersist(label, o)]
+      : [].concat(
+          ...Object.keys(o).map(key =>
+            createPersistTree(label + '-' + key, o[key])
+          )
+        )
+
+  const createReadTree = (label, o) => () =>
+    typeof o === 'function'
+      ? read(label)
+      : Object.keys(o).reduce((u, key) => ({
+          ...u,
+          [key]: createReadTree(label + '-' + key, o[key]),
+        }))
+
+  const persistList = createPersistTree(rootLabel, o)
+  const read = createReadTree(rootLabel, o)
+
+  return {
+    write: state => persistList.forEach(fn => fn(state)),
+    read,
+  }
+}
 
 export const init = store => {
-  const update = () => {
-    const state = store.getState()
-    write('watwet-user', selectCurrentUser(state))
-    write('watwet-resource', extractStorableResource(state.resource))
-  }
+  const { write, read } = createPersistObject('watwet', {
+    user: selectCurrentUser,
+    resource: {
+      original: state => state.resource.original,
+      mutated: state => state.resource.mutated,
+      dateFetched: state => state.resource.dateFetched,
+      dateMutated: state => state.resource.dateMutated,
+    },
+  })
 
-  store.dispatch(
-    readAction({
-      user: read('watwet-user'),
-      resource: read('watwet-resource'),
-    })
-  )
+  const update = debounce(1000)(() => {
+    let start = Date.now()
+
+    write(store.getState())
+
+    console.log(Date.now() - start, 'ms')
+  })
+
+  store.dispatch(readAction(read()))
 
   update()
 
--- src/util/time.js
+++ src/util/time.js
@@ -4,3 +4,39 @@ export const wait = (delay: number = 0): Promise<void> =>
 
     onCancel && onCancel(clearTimeout(killTimeout))
   })
+
+export const throttle = (delay: number = 0) => fn => {
+  let pending = null
+
+  const exec = () => {
+    pending = null
+    fn()
+  }
+
+  const out = () => {
+    if (pending) return
+
+    pending = setTimeout(exec, delay)
+  }
+
+  out.cancel = () => clearTimeout(pending)
+
+  return out
+}
+
+export const debounce = (delay: number) => fn => {
+  let pending = null
+  let _args = null
+
+  const exec = () => fn(..._args)
+
+  const out = (...args) => {
+    _args = args
+    clearTimeout(pending)
+    pending = setTimeout(exec, delay)
+  }
+
+  out.cancel = () => clearTimeout(pending)
+
+  return out
+}
