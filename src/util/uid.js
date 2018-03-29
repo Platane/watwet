d6a48ed3f803890a6e0c15e81d8965cce1986999
platane <me@platane.me>
2018-03-29 13:36:58 +0100

    :rocket: better uid generator

--- src/util/uid.js
+++ src/util/uid.js
@@ -1,6 +1,13 @@
 export const genUid = () =>
-  Math.random()
-    .toString(16)
-    .slice(2)
+  Array.from({ length: 10 })
+    .reduce(
+      sum =>
+        sum +
+        Math.random()
+          .toString(36)
+          .slice(2),
+      ''
+    )
+    .slice(0, 32)
 
 export const genIUid = () => 0 | (Math.random() * (1 << 30))
