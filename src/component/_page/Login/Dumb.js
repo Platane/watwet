24f7a8a2ad71206715f58a121caf4717cc9fad29
platane <me@platane.me>
2018-04-10 18:02:16 +0100

    :rocket: splash screen info

--- src/component/_page/Login/Dumb.js
+++ src/component/_page/Login/Dumb.js
@@ -1,18 +1,48 @@
 import { h, Component } from 'preact'
 import styled from 'preact-emotion'
-import { variant, white } from '~/component/_abstract/palette'
+import { vibrant, white } from '~/component/_abstract/palette'
 
 export const Login = ({ pending, login }) => (
   <Container>
+    <Info>
+      <InfoTitle>Hello here</InfoTitle>
+      <InfoLine>
+        This app is meant to help you set up a list of vegetals.
+      </InfoLine>
+      <InfoLine>
+        Every list you build will be persisted in your google Drive.
+      </InfoLine>
+      <InfoLine>That's why we first need you to login.</InfoLine>
+    </Info>
+
     {!pending && (
       <Center>
-        <Big>:(</Big>
         <Label>Please login to your google account</Label>
         <Button onClick={login}>Login</Button>
       </Center>
     )}
   </Container>
 )
+
+const Info = styled.section`
+  width: calc(100% - 40px);
+  max-width: 400px;
+  background-color: ${white};
+  padding: 32px;
+  border-radius: 4px;
+  box-shadow: 2px 1px 4px 0px rgba(0, 0, 0, 0.3);
+  margin-bottom: 64px;
+`
+const InfoTitle = styled.h1`
+  margin-bottom: 1em;
+`
+const InfoLine = styled.p`
+  margin-bottom: 1em;
+
+  &:last-child {
+    margin-bottom: auto;
+  }
+`
 
 const Container = styled.div`
   z-index: 2;
@@ -22,11 +52,11 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex: 10px 1 1;
+  min-height: 600px;
 `
 
 const Center = styled.div`
-  width: 80%;
-  max-width: 300px;
+  min-height: 100px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -36,12 +66,13 @@ const Center = styled.div`
 const Big = styled.div`
   font-size: 80px;
 `
-const Button = styled.div`
-  margin-top: 40px;
+const Button = styled.button`
+  margin-top: 16px;
 
-  background-color: ${variant[2]};
+  background-color: ${vibrant[2]};
   color: ${white};
   font-size: 40px;
+  border: none;
   box-shadow: rgba(0, 0, 0, 0.3) 2px 1px 4px 0px;
   text-align: center;
   padding: 20px;
