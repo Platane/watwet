9a1fceb712bb67138f21684d91d0ba36a16c5c86
platane <me@platane.me>
2018-12-01 14:14:40 +0000

    :rocket: add reselect-tools for debugging purpose

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'unfetch/polyfill'
 import Raven from 'raven-js'
 import { SENTRY_DSN } from '~/config'
 
-import { create } from './store/index'
+import { create as createStore } from './store/index'
 
 import { init as initUi } from '~/sideEffect/ui'
 import { init as initAuth } from '~/sideEffect/auth'
@@ -13,6 +13,9 @@ import { init as initOnlineStorage } from '~/sideEffect/onlineStorage'
 import { init as initServiceWorker } from '~/sideEffect/serviceWorker'
 import { init as initSentryIdentity } from '~/sideEffect/sentryIdentity'
 import { init as initOfflineDetector } from '~/sideEffect/offlineDetector'
+
+import * as ReselectTools from 'reselect-tools'
+import * as selectors from './store/selector'
 
 // init raven
 if (SENTRY_DSN) {
@@ -34,4 +37,16 @@ const sideEffects = [
   initOfflineDetector,
   initUi,
 ]
-Raven.context(() => create(sideEffects))
+Raven.context(() => {
+  const store = createStore(sideEffects)
+
+  ReselectTools.getStateWith(() => store.getState())
+})
+
+// init reselect-tools
+const renameSelectors = o => {
+  const c = {}
+  Object.keys(o).forEach(key => (c[key.match(/^(select)?(.*)$/)[2]] = o[key]))
+  return c
+}
+ReselectTools.registerSelectors(renameSelectors(selectors))
