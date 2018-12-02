2a48f268bdeb71980dc31818c719c150fa355604
platane <me@platane.me>
2018-12-02 13:01:55 +0000

    :hammer: prevent syncindicator to animate when chaning route

--- src/component/Header/SyncIndicator/Label/index.js
+++ src/component/Header/SyncIndicator/Label/index.js
@@ -3,12 +3,14 @@ import {
   selectResourceLastSyncDate,
   selectResourceFetching,
   selectResourceMutated,
+  selectResourceKey,
 } from '../selector'
 import { selectOffline } from '~/store/selector/offline'
 
 import { Label as Dumb } from './Dumb'
 
 const withState = connect(state => ({
+  key: selectResourceKey(state),
   offline: selectOffline(state),
   mutated: selectResourceMutated(state),
   fetching: selectResourceFetching(state),
