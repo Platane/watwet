c4e281738d933204c19e2a32d5c3a192bd417a01
platane <me@platane.me>
2018-12-02 10:59:22 +0000

    :rocket: refacto entity getter side effect

---
+++ src/sideEffect/onlineStorage/get/getEntity.js
@@ -0,0 +1,37 @@
+import {
+  listId as listSitesId,
+  get as getSite,
+} from '~/service/google-api/spreadsheets/site'
+import { get as getHabitatDirectory } from '~/service/google-api/spreadsheets/habitatDirectory'
+import { get as getVegetalDictionary } from '~/service/google-api/spreadsheets/vegetalDictionary'
+import { normalizeSite } from '~/service/normalize'
+
+/**
+ * get any entity
+ * delegate to the relevant implementation depending on the type
+ */
+export const getEntity = async (key: string, getState: *) => {
+  const [entity, id] = key.split('.', 2)
+
+  switch (entity) {
+    case 'habitatDictionary':
+      return {
+        habitatDictionary: await getHabitatDirectory(
+          getState().setting.habitatDictionarySpreadsheetId
+        ),
+      }
+
+    case 'vegetalDictionary':
+      return {
+        vegetalDictionary: await getVegetalDictionary(
+          getState().setting.vegetalDictionarySpreadsheetId
+        ),
+      }
+
+    case 'sites':
+      return { sites: (await listSitesId()).map(id => `site.${id}`) }
+
+    case 'site':
+      return normalizeSite(await getSite(id))
+  }
+}
---
+++ src/sideEffect/onlineStorage/get/index.js
@@ -0,0 +1,59 @@
+import { hydrate, fetchError } from '~/store/action/onlineStorage'
+import { selectSpreadsheetApiReady } from '~/store/selector/spreadsheetApiReady'
+import { getEntity } from './getEntity'
+
+export const init = store => {
+  let pending = {}
+  let failed = {}
+
+  const update = async () => {
+    const state = store.getState()
+
+    /**
+     * if the api is not ready, ignore
+     */
+    if (!selectSpreadsheetApiReady(state)) return
+
+    /**
+     * get the next thing that should be fetched
+     * which is not currently pending
+     * or which have not already failed
+     */
+    const { shouldFetch } = state.resource
+    const key = Object.keys(shouldFetch).filter(
+      key => !pending[key] && failed[key] !== shouldFetch[key]
+    )[0]
+
+    if (!key) return
+
+    /**
+     * mark it as pending
+     */
+    const shouldFetchKey = shouldFetch[key]
+    pending[key] = shouldFetchKey
+
+    /**
+     * fetch the entity
+     * and dispatch the hydrate event
+     * or fails and dispatch an error event
+     */
+    try {
+      const res = await getEntity(key, store.getState)
+      store.dispatch(hydrate(res))
+    } catch (error) {
+      failed[key] = shouldFetchKey
+      store.dispatch(fetchError(error, key, shouldFetchKey))
+    }
+
+    /**
+     * mark it as not longer pending
+     * and loop to exhaust the list of thing that should be fetched
+     */
+    pending[key] = null
+    update()
+  }
+
+  update()
+
+  store.subscribe(update)
+}
