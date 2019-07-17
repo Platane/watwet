0853f38d2298c1f6b0537d359ff3cb999d857084
werckerbot <pleasemailus@wercker.com>
2019-07-17 08:09:11 +0000

    deploy from platane

---
+++ sw.js
@@ -0,0 +1,46 @@
+/* global caches self URL fetch */
+const r = '/watwet/'.replace(/\/$/, '');
+const assets = [//
+r, r + '/', '/watwet/index.html', '/watwet/index-39464289.js'];
+const hostname = "platane.github.io" || 'localhost';
+const assetCacheKey = assets.join('-').replace(/\//g, '');
+const staticCacheKey = 'static';
+const imageCacheKey = 'image';
+const dataCacheKey = 'data';
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(assetCacheKey).then(cache => cache.addAll(assets)).catch(err => console.error(err)));
+});
+self.addEventListener('activate', event => {
+  const whiteList = [assetCacheKey];
+  event.waitUntil( // get the currently cached files, remove the one that are out of date
+  caches.keys().then(cacheKeys => {
+    Promise.all(cacheKeys.map(key => !whiteList.includes(key) && caches.delete(key)));
+  }).catch(err => console.error(err)));
+});
+
+const cacheFirstStrategy = cacheName => async request => {
+  const resFromCache = await caches.match(request);
+  if (resFromCache) return resFromCache;
+  const resFromFetch = await fetch(request.clone());
+  const cache = await caches.open(cacheName);
+  cache.put(request, resFromFetch.clone());
+  return resFromFetch;
+};
+
+const networkFirstStrategy = cacheName => request => fetch(request.clone()).then(async resFromFetch => {
+  const cache = await caches.open(cacheName);
+  cache.put(request, resFromFetch.clone());
+  return resFromFetch;
+}).catch(async err => {
+  const resFromCache = await caches.match(request);
+  return resFromCache || Promise.reject(err);
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const requestURL = new URL(event.request.url); // cache every image
+
+  if (requestURL.pathname.match(/\.(png|jpg|gif|webp|svg)$/)) return event.respondWith(cacheFirstStrategy(imageCacheKey)(event.request));
+  if (hostname === requestURL.hostname && assets.includes(requestURL.pathname)) // cached as asset
+    return event.respondWith(caches.match(event.request));
+});
